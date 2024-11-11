3753cc34d4fc48411492f56dfe25e747d4ba9a99
RoberCraudio <robercraudio@gmail.com>
2024-11-11 00:20:50 -0300

    refatoracao validacao formulario partida

--- src/app/service/partidaService.go
+++ src/app/service/partidaService.go
@@ -17,23 +17,32 @@ func UpdatePartida(id string, partida model.Partida) error {
 }
 
 func CreatePartida(partidaCreate model.PartidaCreate) error {
-	jogouNaRodada := repository.JogouNaRodada(partidaCreate.TimeCasa, partidaCreate.TimeFora, partidaCreate.Rodada)
 
-	if jogouNaRodada {
-		errMsg := "Os times já jogaram nesta rodada."
-		log.Printf("Erro: %s", errMsg)
-		return errors.New(strings.ToLower(errMsg))
-	}
-
-	if partidaCreate.TimeCasa == partidaCreate.TimeFora {
-		errMsg := "Os times selecionados são iguais."
-		return errors.New(strings.ToLower(errMsg))
+	if err := ValidacoesFormulario(partidaCreate); err != nil {
+		return err
 	}
 
 	err := repository.CreatePartida(partidaCreate)
 	if err != nil {
 		log.Printf("Erro ao criar partida: %v", err)
 		return err
+	}
+
+	return nil
+}
+
+func ValidacoesFormulario(partidaCreate model.PartidaCreate) error {
+
+	if partidaCreate.TimeCasa == partidaCreate.TimeFora {
+		errMsg := "Os times selecionados são iguais."
+		return errors.New(strings.ToLower(errMsg))
+	}
+
+	jogouNaRodada := repository.JogouNaRodada(partidaCreate.TimeCasa, partidaCreate.TimeFora, partidaCreate.Rodada)
+	if jogouNaRodada {
+		errMsg := "Um dos times já jogou nesta rodada."
+		log.Printf("Erro: %s", errMsg)
+		return errors.New(strings.ToLower(errMsg))
 	}
 
 	return nil
